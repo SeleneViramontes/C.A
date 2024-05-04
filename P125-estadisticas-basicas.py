333720000b71952257d7161775bdb28d75eddaf7
SeleneViramontes <35164524@uaz.edu.mx>
2024-05-03 22:48:03 -0600

    Tarea 9

--- P125-estadisticas-basicas.py
+++ P125-estadisticas-basicas.py
@@ -6,6 +6,7 @@ def arreglos():
 
 def mayor(lista): 
     m = lista[0] 
+    
     for n in range(len(lista)): 
         if lista[n]>m:
             m=lista[n]
@@ -24,15 +25,11 @@ def media(lista):
         return med
     
 def varianza(lista):
-    if len(lista) == 0:
-        return None
     med = media(lista)
     var = sum((x - med) ** 2 for x in lista) / len(lista)
     return var
 
 def desviacion_estandar(lista):
-    if len(lista) == 0:
-        return None
     var = varianza(lista)
     des_estandar = var ** 0.5
     return des_estandar
@@ -42,20 +39,17 @@ numeros = arreglos()
 print("Lista original:", numeros)
 
 may=mayor(numeros)
-men= menor(numeros)
-
-print(f"Los numeros son: {numeros}, {len(numeros)}")
-print("El mayor es", mayor)
-print("El menor es", menor)
-#-----------------------------------------------
+men=menor(numeros)
 med = media(numeros)
-if med is not None:
-    print("Media:", med)
-#-----------------------------------------------
 var = varianza(numeros)
-if var is not None:
-    print("Varianza:", var)
-#----------------------------------------------
 des_estandar = desviacion_estandar(numeros)
-if des_estandar is not None:
-    print("Desviación estándar:", des_estandar)
+
+print("El mayor es", may)
+#----------------------------------------------
+print("El menor es", men)
+#-----------------------------------------------
+print(f'Media: {med:.3f}')
+#-----------------------------------------------
+print(f'Varianza: {var:.3f}')
+#----------------------------------------------
+print(f'Desviación estándar : {des_estandar:.3f}')
